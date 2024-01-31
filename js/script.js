cc06c438880244392918e38d8356c462251fc3ac
raldybn <raldybelo@gmail.com>
2024-01-31 21:27:45 +0700

    perubahan ke 2

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 //toggle class active
 const navbarNav = document.querySelector(".navbar-nav");
 //ketika product menu di klik
-document.querySelector("#product-menu").onclick = () => {
+const product = document.getElementById("#product-menu");
+product.onclick = () => {
   navbarNav.classList.toggle("active");
 };
-
 // klik diluar side bar untuk menghilangkan nav
-const product = document.querySelector("#product-menu");
-
+//const product = document.querySelector("#product-menu");
+//produk=document.queryselector
 document.addEventListener("click", function (e) {
   if (!product.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
 });
+console.log(navbarNav);
+
+const slideshow = [{title:"Together Developing A Healthy life", slogan:"In giving excellent service, Jafarel will focus its competency in diagnostic and distribution of medical paraphernalia to support medical examination in laboratories across Indonesia".}]
