ac2e8984a3c0b3f9af0dbf5baba7d441d27bb3e1
raldybn <raldybelo@gmail.com>
2024-03-06 14:21:16 +0700

    pembaruan

--- js/script.js
+++ js/script.js
@@ -41,8 +41,3 @@ showSlides(slideIndex);
 function plusSlides(n) {
   showSlides((slideIndex += n));
 }
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
