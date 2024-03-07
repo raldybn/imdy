4ffbf69e362b5584bb4d8257d6e7265ea5eb0f36
raldybn <raldybelo@gmail.com>
2024-03-07 22:01:09 +0700

    pembaruan

--- js/script.js
+++ js/script.js
@@ -32,6 +32,34 @@
 //   }
 // });
 
+const navbar2 = document.getElementById("navbar2");
+navbar2.style.top = "30%";
+function boxKekanan() {
+  // navbar2.style.top = "30%";
+  navbar2.style.right = "15px";
+
+  var elem = document.getElementById("navbar2");
+  var pos = 25; //jarak tempuh
+  // clearInterval(navbar2);
+  navbar2 = setInterval(frame, 1); //waktu transisi
+  function frame() {
+    if (pos == 200) {
+      //jarak tempuh
+    } else {
+      pos++;
+      elem.style.top = pos + "-100px";
+      elem.style.left = pos + "px";
+    }
+  }
+}
+
+// const navbar2 = document.getElementById("navbar2");
+// document.addEventListener("click", function (e) {
+//   if (!navbar2.contains(e.target)) {
+//     navbar2.classList.remove("active");
+//   }
+// });
+
 const hamburgerMenu = document.getElementById("hamburger-menu");
 document.getElementById("hamburger-menu").onclick = (e) =>
   function myFunction() {
