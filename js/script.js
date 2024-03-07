2562c06b9a27392d5415e2a6d412bd49510e050e
raldybn <raldybelo@gmail.com>
2024-03-07 15:21:47 +0700

    pembaruan

--- js/script.js
+++ js/script.js
@@ -1,37 +1,42 @@
-//toggle class active product menu
-const navbarNav = document.querySelector(".navbar-nav");
-//ketika product menu di klik
-const product = document.querySelector("#hamburger-menu");
+// //toggle class active product menu
+// const navbarNav = document.querySelector(".navbar-nav");
+// //ketika product menu di klik
+// const product = document.querySelector("#hamburger-menu");
 
-product.addEventListener("click", () => {
-  navbarNav.classList.toggle("active");
-});
+// product.addEventListener("click", () => {
+//   navbarNav.classList.toggle("active");
+// });
 
-// toggle class active search form
-const seacrhForm = document.querySelector(".search-form");
-const searchBox = document.querySelector("#search-box");
+// // toggle class active search form
+// const seacrhForm = document.querySelector(".search-form");
+// const searchBox = document.querySelector("#search-box");
 
-document.querySelector("#search-button").onclick = (e) => {
-  seacrhForm.classList.toggle("active");
-  searchBox.focus();
-  e.preventDefault();
-};
+// document.querySelector("#search-button").onclick = (e) => {
+//   seacrhForm.classList.toggle("active");
+//   searchBox.focus();
+//   e.preventDefault();
+// };
 
-// klik diluar elemen
-const hm = document.querySelector("#hamburger-menu");
-const sb = document.querySelector("#search-button");
-const button = document.querySelector(".button");
-document.addEventListener("click", function (e) {
-  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove("active");
-  }
-  if (!sb.contains(e.target) && !seacrhForm.contains(e.target)) {
-    seacrhForm.classList.remove("active");
-  }
-  if (!button.contains(e.target) && !tolls.contains(e.target)) {
-  }
-});
+// // klik diluar elemen
+// const hm = document.querySelector("#hamburger-menu");
+// const sb = document.querySelector("#search-button");
+// const button = document.querySelector(".button");
+// document.addEventListener("click", function (e) {
+//   if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+//     navbarNav.classList.remove("active");
+//   }
+//   if (!sb.contains(e.target) && !seacrhForm.contains(e.target)) {
+//     seacrhForm.classList.remove("active");
+//   }
+//   if (!button.contains(e.target) && !tolls.contains(e.target)) {
+//   }
+// });
 
+const hamburgerMenu = document.getElementById("hamburger-menu");
+document.getElementById("hamburger-menu").onclick = (e) =>
+  function myFunction() {
+    document.getElementById("hamburger-menu").innerHTML = "fuck";
+  };
 // slideshow #shopping.html
 
 let slideIndex = 1;
