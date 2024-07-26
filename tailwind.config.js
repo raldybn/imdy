ab8d2a6796e47ea9b7d3ad81645912f5e4719352
raldybn <raldybelo@gmail.com>
2024-07-26 11:50:38 +0700

    pembaruan

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#13072E',
+        dark: '#00202e',
+        orange: '#ffa600',
+        blue: '#2c4875',
+      }
+    },
+  },
+  plugins: [],
+}
+
